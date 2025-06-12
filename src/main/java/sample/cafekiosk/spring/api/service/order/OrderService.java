b798991e90abad7de279fd0c0819dd375e92eae8
Han SeokHyeon <westhan98@gmail.com>
2025-06-12 23:47:32 +0900

    예외 케이스 테스트 성공 - TDD:GREEN

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -11,6 +11,8 @@ import sample.cafekiosk.spring.domain.product.Product;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -22,8 +24,14 @@ public class OrderService {
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
         List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductNumber, p -> p));
 
-        Order order = Order.create(products, registeredDateTime);
+        List<Product> duplicateProducts = productNumbers.stream()
+                .map(productMap::get)
+                .collect(Collectors.toList());
+
+        Order order = Order.create(duplicateProducts, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
 
         return OrderResponse.of(savedOrder);
