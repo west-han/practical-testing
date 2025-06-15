3369c69517eec7ce05a0d5e38902b2cbbc292308
Han SeokHyeon <westhan98@gmail.com>
2025-06-15 18:20:30 +0900

    기능 구현, 테스트 코드 성공 확인, 예외 케이스 테스트코드 작성 - TDD:GREEN

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -4,8 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
-import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 
 import java.util.List;
@@ -17,9 +17,15 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     public ProductResponse createProduct(ProductCreateRequest request) {
-        String lastestProductNumber = productRepository.findLatestProductNumber();
+        String nextProductNumber = createNextProductNumber();
 
-        return null;
+        return ProductResponse.builder()
+                .productNumber(nextProductNumber)
+                .type(request.getType())
+                .sellingStatus(request.getSellingStatus())
+                .name(request.getName())
+                .price(request.getPrice())
+                .build();
     }
 
     public List<ProductResponse> getSellingProducts() {
@@ -28,5 +34,18 @@ public class ProductService {
         return products.stream()
                 .map(ProductResponse::of)
                 .toList();
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        return String.format("%03d", nextProductNumberInt);
     }
 }
--- src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
@@ -1,9 +1,11 @@
 package sample.cafekiosk.spring.api.service.product;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
@@ -15,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
 import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ProductServiceTest {
 
@@ -24,12 +27,17 @@ class ProductServiceTest {
     @Autowired
     private ProductRepository productRepository;
 
+    @AfterEach
+    void tearDown() {
+        productRepository.deleteAllInBatch();
+    }
+
     @DisplayName("신규 상품을 등록한다. 상품번호는 가장 최근 상품의 상품번호에서 1 증가한 값이다.")
     @Test
     void createProduct() {
         // given
-        Product product1 = createProduct("001", HANDMADE, SELLING, "아메리카노", 4000);
-        productRepository.save(product1);
+        Product product = createProduct("001", HANDMADE, SELLING, "아메리카노", 4000);
+        productRepository.save(product);
 
         ProductCreateRequest request = ProductCreateRequest.builder()
                 .type(HANDMADE)
@@ -45,6 +53,26 @@ class ProductServiceTest {
         assertThat(productResponse)
                 .extracting("productNumber", "type", "sellingStatus", "name", "price")
                 .contains("002", HANDMADE, SELLING, "카푸치노", 5000);
+    }
+
+    @DisplayName("상품이 하나도 없는 경우, 신규 상품을 등록하면 상품번호는 001이다.")
+    @Test
+    void createProductWhenProductIsEmpty() {
+        // given
+        ProductCreateRequest request = ProductCreateRequest.builder()
+                .type(HANDMADE)
+                .sellingStatus(SELLING)
+                .name("카푸치노")
+                .price(5000)
+                .build();
+
+        // when
+        ProductResponse productResponse = productService.createProduct(request);
+
+        // then
+        assertThat(productResponse)
+                .extracting("productNumber", "type", "sellingStatus", "name", "price")
+                .contains("001", HANDMADE, SELLING, "카푸치노", 5000);
     }
 
     private Product createProduct(String productNumber, ProductType productType, ProductSellingStatus sellingStatus, String name, int price) {
